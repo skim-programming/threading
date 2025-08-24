13891ea6900f7df538b16f8d2522e2387a257c2f
keepcry1ng <skimprogramming@gmail.com>
2025-08-23 20:28:00 -0700

    Create thread_pool.h

---
+++ thread_pool.h
@@ -0,0 +1,60 @@
+// Header for multithreading/thread operations, general use
+#pragma once
+#include <vector>
+#include <thread>
+#include <queue>
+#include <functional>
+#include <condition_variable>
+#include <mutex>
+#include <future>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t threads = std::thread::hardware_concurrency()) {
+        for (size_t i = 0; i < threads; ++i) {
+            workers.emplace_back([this] {
+                for (;;) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(queue_mutex);
+                        cv.wait(lock, [this] { return stop || !tasks.empty(); });
+                        if (stop && tasks.empty()) return;
+                        task = std::move(tasks.front());
+                        tasks.pop();
+                    }
+                    task();
+                }
+                });
+        }
+    }
+
+    template<class F>
+    auto enqueue(F&& f) -> std::future<decltype(f())> {
+        using return_type = decltype(f());
+        auto task = std::make_shared<std::packaged_task<return_type()>>(std::forward<F>(f));
+        std::future<return_type> res = task->get_future();
+        {
+            std::lock_guard<std::mutex> lock(queue_mutex);
+            tasks.emplace([task]() { (*task)(); });
+        }
+        cv.notify_one();
+        return res;
+    }
+
+
+    ~ThreadPool() {
+        {
+            std::lock_guard<std::mutex> lock(queue_mutex);
+            stop = true;
+        }
+        cv.notify_all();
+        for (std::thread& worker : workers) worker.join();
+    }
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queue_mutex;
+    std::condition_variable cv;
+    bool stop = false;
+};
